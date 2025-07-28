85b42e81ce52529c42f4db966f5229e17cd53e6b
Armanwarraich <armanwarraich4496@gmail.com>
2025-07-28 12:06:34 +0530

    Final Commit

--- send_expiry_notifications.py
+++ send_expiry_notifications.py
@@ -13,7 +13,7 @@ MONGO_URI = os.environ.get("MONGO_URI")
 if not MONGO_URI:
     raise ValueError("❌ MONGO_URI environment variable not set.")
 
-DB_NAME = "grocery_db"
+DB_NAME = "grocery_tracker"
 COLLECTION_NAME = "products"
 USERS_COLLECTION = "users"  # Assumes you have a 'users' collection with emails
 
